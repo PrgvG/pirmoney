9ad60939e4a8c6339fab51aa2c008aefa11593ef
German Pirogov <pirogov.g@gmail.com>
2025-03-15 12:01:55 +0300

    wip

--- frontend/src/features/add_moment_payment/add_moment_payment.tsx
+++ frontend/src/features/add_moment_payment/add_moment_payment.tsx
@@ -1,12 +1,11 @@
 import { FC, useRef } from 'react';
 import styles from './add_moment_payment.module.css';
 
-import { OneTimePayment, paymentApi } from '../../entities';
-
-import { MomentPaymentForm } from './components/moment_payment_form/moment_payment_form';
+import { OneTimePayment, paymentApi, useCategories } from '../../entities';
 
 import { nanoid } from 'nanoid';
-import { Button } from 'antd';
+import { Button, Form, Input, InputNumber, Switch } from 'antd';
+import { Controller, useForm } from 'react-hook-form';
 
 type Props = {
     onAdd(data: OneTimePayment): void;
@@ -14,6 +13,18 @@ type Props = {
 
 export const AddMomentPaymentButton: FC<Props> = ({ onAdd }) => {
     const dialogRef = useRef<HTMLDialogElement>(null);
+
+    const { categories, hasCategories } = useCategories();
+
+    const { handleSubmit, register, reset, control, formState, setValue } =
+        useForm<OneTimePayment>({
+            defaultValues: {
+                payment_kind: 'outcome',
+                payment_type: 'one_time_payment',
+                payment_date: new Date(),
+                completed_at: new Date(),
+            },
+        });
 
     const handleOpenDialog = () => {
         dialogRef.current?.showModal();
@@ -23,11 +34,10 @@ export const AddMomentPaymentButton: FC<Props> = ({ onAdd }) => {
         dialogRef.current?.close();
     };
 
-    const handleSubmit = (data: OneTimePayment) => {
+    const handleSubmitForm = (data: OneTimePayment) => {
         Object.assign(data, {
             _id: nanoid(),
         });
-        handleCloseDialog();
         // TODO: на catch сделать откат данных
         paymentApi.savePayment(data);
         onAdd(data);
@@ -40,26 +50,93 @@ export const AddMomentPaymentButton: FC<Props> = ({ onAdd }) => {
             </Button>
 
             <dialog ref={dialogRef}>
-                <div className={styles.dialog}>
-                    <MomentPaymentForm
-                        onReset={handleCloseDialog}
-                        onSubmit={handleSubmit}
-                        formId="addMomentPaymentForm"
-                    />
-
+                <form
+                    method="dialog"
+                    className={styles.form}
+                    onSubmit={handleSubmit((data) => {
+                        handleSubmitForm(data);
+                        reset();
+                    })}
+                    onReset={() => {
+                        handleCloseDialog();
+                        reset();
+                    }}
+                >
+                    <section>
+                        <Controller
+                            name="label"
+                            rules={{ required: true }}
+                            control={control}
+                            render={({ field }) => (
+                                <Form.Item label="Название">
+                                    <Input
+                                        size="middle"
+                                        autoComplete="off"
+                                        readOnly={formState.isSubmitting}
+                                        {...field}
+                                    />
+                                </Form.Item>
+                            )}
+                        />
+                        <Controller
+                            name="payment_amount"
+                            rules={{ required: true }}
+                            control={control}
+                            render={({ field }) => (
+                                <Form.Item label="Сумма платежа">
+                                    <InputNumber
+                                        style={{ width: '100%' }}
+                                        size="middle"
+                                        step={0.01}
+                                        min={0}
+                                        inputMode="decimal"
+                                        readOnly={formState.isSubmitting}
+                                        {...field}
+                                    />
+                                </Form.Item>
+                            )}
+                        />
+                        {hasCategories && (
+                            <label className={styles.selectField}>
+                                Категория:
+                                <select
+                                    className={styles.select}
+                                    {...register('category_id', {
+                                        required: true,
+                                    })}
+                                >
+                                    {categories.map((category) => (
+                                        <option
+                                            key={category._id}
+                                            value={category._id}
+                                        >
+                                            {category.name}
+                                        </option>
+                                    ))}
+                                </select>
+                            </label>
+                        )}
+                        <Form.Item label="Тип транзакции">
+                            <Switch
+                                checkedChildren="Расход"
+                                unCheckedChildren="Приход"
+                                defaultChecked
+                                onChange={(e) => {
+                                    setValue(
+                                        'payment_kind',
+                                        e.valueOf() ? 'outcome' : 'income',
+                                    );
+                                }}
+                            />
+                        </Form.Item>
+                    </section>
                     <div className={styles.controls}>
-                        <Button
-                            htmlType="submit"
-                            type="primary"
-                            form="addMomentPaymentForm"
-                        >
+                        <Button htmlType="submit" type="primary">
                             Добавить
                         </Button>
-                        <Button htmlType="reset" form="addMomentPaymentForm">
-                            Закрыть
-                        </Button>
+                        <Button htmlType="reset">Закрыть</Button>
                     </div>
-                </div>
+                </form>
             </dialog>
         </>
     );
--- frontend/src/features/add_moment_payment/components/moment_payment_form/moment_payment_form.tsx
+++ frontend/src/features/add_moment_payment/components/moment_payment_form/moment_payment_form.tsx
@@ -17,94 +17,9 @@ export const MomentPaymentForm: FC<Props> = ({
     onReset,
     initialValues,
 }) => {
-    const { categories, hasCategories } = useCategories();
-
-    const { handleSubmit, register, reset, control, formState, setValue } =
-        useForm<OneTimePayment>({
-            defaultValues: initialValues || {
-                payment_kind: 'outcome',
-                payment_type: 'one_time_payment',
-                payment_date: new Date(),
-                completed_at: new Date(),
-            },
-        });
+    
 
     return (
-        <form
-            id={formId}
-            className={styles.form}
-            onSubmit={handleSubmit((data) => {
-                onSubmit(data);
-                reset();
-            })}
-            onReset={() => {
-                onReset();
-                reset();
-            }}
-        >
-            <Controller
-                name="label"
-                rules={{ required: true }}
-                control={control}
-                render={({ field }) => (
-                    <Form.Item label="Название">
-                        <Input
-                            size="middle"
-                            autoComplete="off"
-                            readOnly={formState.isSubmitting}
-                            {...field}
-                        />
-                    </Form.Item>
-                )}
-            />
-            <Controller
-                name="payment_amount"
-                rules={{ required: true }}
-                control={control}
-                render={({ field }) => (
-                    <Form.Item label="Сумма платежа">
-                        <InputNumber
-                            style={{ width: '100%' }}
-                            size="middle"
-                            step={0.01}
-                            min={0}
-                            inputMode="decimal"
-                            readOnly={formState.isSubmitting}
-                            {...field}
-                        />
-                    </Form.Item>
-                )}
-            />
-
-            {hasCategories && (
-                <label className={styles.selectField}>
-                    Категория:
-                    <select
-                        className={styles.select}
-                        {...register('category_id', { required: true })}
-                    >
-                        {categories.map((category) => (
-                            <option key={category._id} value={category._id}>
-                                {category.name}
-                            </option>
-                        ))}
-                    </select>
-                </label>
-            )}
-
-            <Form.Item label="Тип транзакции">
-                <Switch
-                    checkedChildren="Расход"
-                    unCheckedChildren="Приход"
-                    defaultChecked
-                    onChange={(e) => {
-                        setValue(
-                            'payment_kind',
-                            e.valueOf() ? 'outcome' : 'income',
-                        );
-                    }}
-                />
-            </Form.Item>
-        </form>
+        
     );
 };
