90643cb20295415111ed41d2e5c35d59d0d3f54b
German Pirogov <pirogov.g@gmail.com>
2025-03-16 13:26:20 +0300

    fix context

--- frontend/src/context/mobile.tsx
+++ frontend/src/context/mobile.tsx
@@ -2,14 +2,16 @@ import { createContext, FC, PropsWithChildren, useContext } from 'react';
 
 const IsMobileContext = createContext(false);
 
-export const IsMobileProvider: FC<PropsWithChildren> = () => {
+export const IsMobileProvider: FC<PropsWithChildren> = ({ children }) => {
     const isMobile =
         /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
             navigator.userAgent,
         );
 
     return (
-        <IsMobileContext.Provider value={isMobile}></IsMobileContext.Provider>
+        <IsMobileContext.Provider value={isMobile}>
+            {children}
+        </IsMobileContext.Provider>
     );
 };
 
