121475422de081bc7b22635489c421b5dc376a8d
German Pirogov <pirogov.g@gmail.com>
2025-03-16 22:51:56 +0300

    wip

--- frontend/src/features/payments_grid/model/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { Row } from '@tanstack/react-table';
-import { Payment } from '../../../entities';
-
-export function isSeparatorRow(row: Row<Payment>): boolean {
-    return row.original._id === 'separator';
-}
